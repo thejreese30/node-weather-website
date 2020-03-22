43466caccf1c09475ca180085cb31bdb053f7c57
Justin Reese <jdreese3002@gmail.com>
2020-03-21 20:39:06 -0400

    App setup for heroku

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
  const path = require('path')
- const express = require('express') 
-const app = express()     
+ const express = require('express')     
 const hbs = require('hbs')  
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const app = express()  
 
+const port = process.env.PORT || 3000
 
 const viewsPath = path.join(__dirname, '../templates/views')
 const publicDirectoryPath = path.join(__dirname, '../public')  
@@ -94,6 +95,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => { 
-    console.log('App is running on port 3000')
+app.listen(port, () => { 
+    console.log('App is running on port ' + port)
 })
