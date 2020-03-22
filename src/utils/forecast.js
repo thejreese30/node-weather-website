7fb7db33e339ad8880b06e0802c36115f34c20d3
Justin Reese <jdreese3002@gmail.com>
2020-03-22 14:18:04 -0400

    Added more data to forecast call

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,8 +13,8 @@ const forecast = (latitude, longitude, callback) => {
         
         else { 
             callback(undefined, {
-                // current : body.currently , 
-                // location : body.timezone , 
+                current : body.currently , 
+                location : body.timezone , 
                 forecast : body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain'
                 
             }) 
